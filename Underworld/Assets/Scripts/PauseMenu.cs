0462503822241ab2e4605f47681392a5f1967b89
Signe Grosberga <signe.grosberga@va.lv>
2023-06-03 02:19:14 +0300

    ceru ka kkas aizies jo iepriekšējais commits prasija 4h + beigās viss slikti :)

--- Underworld/Assets/Scripts/PauseMenu.cs
+++ Underworld/Assets/Scripts/PauseMenu.cs
@@ -4,41 +4,48 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-
     public GameObject pauseMenu;
-    public bool isPaused;
+    private bool isPaused;
 
     // Start is called before the first frame update
     void Start()
     {
-       pauseMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-      if(Input.GetKeyDown(KeyCode.Escape)) 
-      {
-        if(isPaused)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ResumeGame();
-        }else{
-            PauseGame();
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
         }
-      }
     }
-
 
     public void PauseGame()
     {
-       pauseMenu.SetActive(true);
+        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
-    }
-     
-    public void ResumeGame()
-    {
-       pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
+        isPaused = true;
     }
 
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void OnResumeButtonClicked()
+    {
+        ResumeGame();
+    }
 }
